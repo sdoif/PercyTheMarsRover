16f2c95b1db5ba41ede14fe026fcf9013a9af6d3
cy1719 <59970706+cy1719@users.noreply.github.com>
2021-06-07 00:52:22 +0100

    tested x y coordinate finder

---
+++ Drive/Combined_cem/Combined_cem.ino
@@ -0,0 +1,370 @@
+#include "SPI.h"
+#include "functions.h"
+#include <Wire.h>
+#include <Console.h>
+
+void setup() 
+{
+  
+    //*** Motor Pins Defining ***//
+  pinMode(DIRR, OUTPUT);
+  pinMode(DIRL, OUTPUT);
+  pinMode(pwmr, OUTPUT);
+  pinMode(pwml, OUTPUT);
+  digitalWrite(pwmr, HIGH);       //setting right motor speed at maximum
+  digitalWrite(pwml, HIGH);       //setting left motor speed at maximum
+  //*********//
+
+  //Basic pin setups
+  
+   //disable all interrupts
+  noInterrupts();
+  //pinMode(13, OUTPUT);  //Pin13 is used to time the loops of the controller
+  pinMode(3, INPUT_PULLUP); //Pin3 is the input from the Buck/Boost switch
+  pinMode(2, INPUT_PULLUP); // Pin 2 is the input from the CL/OL switch
+  analogReference(EXTERNAL); // We are using an external analogue reference for the ADC
+
+  // TimerA0 initialization for control-loop interrupt.
+  
+  TCA0.SINGLE.PER = 999; //255;
+  TCA0.SINGLE.CMP1 = 999; //255; 
+  TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV64_gc | TCA_SINGLE_ENABLE_bm; //16 prescaler, 1M.
+  TCA0.SINGLE.INTCTRL = TCA_SINGLE_CMP1_bm; 
+
+  // TimerB0 initialization for PWM output
+  
+  pinMode(6, OUTPUT);
+  TCB0.CTRLA = TCB_CLKSEL_CLKDIV1_gc | TCB_ENABLE_bm; //62.5kHz
+  analogWrite(6,120); 
+
+  interrupts();  //enable interrupts.
+  Wire.begin(); // We need this for the i2c comms for the current sensor
+  ina219.init(); // this initiates the current sensor
+  Wire.setClock(700000); // set the comms speed for i2c
+  
+  pinMode(PIN_SS,OUTPUT);
+  pinMode(PIN_MISO,INPUT);
+  pinMode(PIN_MOSI,OUTPUT);
+  //pinMode(PIN_SCK,OUTPUT);
+  
+  SPI.begin();
+  SPI.setClockDivider(SPI_CLOCK_DIV32);
+  SPI.setDataMode(SPI_MODE3);
+  SPI.setBitOrder(MSBFIRST);
+  
+  Serial.begin(38400);
+
+  if(mousecam_init()==-1)
+  {
+    Serial.println("Mouse cam failed to init");
+    while(1);
+  }  
+  mousecam_write_reg(11, 7);
+  mousecam_write_reg(25, 126);
+  mousecam_write_reg(26, 14);
+
+}
+  char _mode='x';
+  bool STOP = false;
+  bool DONE;
+
+
+byte frame[ADNS3080_PIXELS_X * ADNS3080_PIXELS_Y];
+
+void loop(){
+  
+  if(loopTrigger) { // This loop is triggered, it wont run unless there is an interrupt
+    
+   //digitalWrite(13, HIGH);   // set pin 13. Pin13 shows the time consumed by each control cycle. It's used for debugging.
+    
+    // Sample all of the measurements and check which control mode we are in
+    sampling();
+    CL_mode = digitalRead(3); // input from the OL_CL switch
+    Boost_mode = digitalRead(2); // input from the Buck_Boost switch
+
+    if (Boost_mode){
+      if (CL_mode) { //Closed Loop Boost
+          pwm_modulate(1); // This disables the Boost as we are not using this mode
+      }else{ // Open Loop Boost
+          pwm_modulate(1); // This disables the Boost as we are not using this mode
+      }
+    }else{      
+      if (CL_mode) { // Closed Loop Buck
+          // The closed loop path has a voltage controller cascaded with a current controller. The voltage controller
+          // creates a current demand based upon the voltage error. This demand is saturated to give current limiting.
+          // The current loop then gives a duty cycle demand based upon the error between demanded current and measured
+          // current
+          current_limit = 3; // Buck has a higher current limit
+          ev = vref - vb;  //voltage error at this time
+          cv=pidv(ev);  //voltage pid
+          cv=saturation(cv, current_limit, 0); //current demand saturation
+          ei=cv-iL; //current error
+          closed_loop=pidi(ei);  //current pid
+          closed_loop=saturation(closed_loop,0.99,0.01);  //duty_cycle saturation
+          pwm_modulate(closed_loop); //pwm modulation
+      }else{ // Open Loop Buck
+          current_limit = 3; // Buck has a higher current limit
+          oc = iL-current_limit; // Calculate the difference between current measurement and current limit
+          if ( oc > 0) {
+            open_loop=open_loop-0.001; // We are above the current limit so less duty cycle
+          } else {
+            open_loop=open_loop+0.001; // We are below the current limit so more duty cycle
+          }
+          open_loop=saturation(open_loop,dutyref,0.02); // saturate the duty cycle at the reference or a min of 0.01
+          pwm_modulate(open_loop); // and send it out
+      }
+    }
+    // closed loop control path
+
+    //digitalWrite(13, LOW);   // reset pin13.
+    loopTrigger = 0;
+  }
+  
+  // if enabled this section produces a bar graph of the surface quality that can be used to focus the camera
+  // also drawn is the average pixel value 0-63 and the shutter speed and the motion dx,dy.
+ 
+  int val = mousecam_read_reg(ADNS3080_PIXEL_SUM);
+  MD md;
+  mousecam_read_motion(&md);
+  for(int i=0; i<md.squal/4; i++)
+    Serial.print('*');
+  Serial.print(' ');
+  Serial.print((val*100)/351);
+  Serial.print(' ');
+  Serial.print(md.shutter); Serial.print(" (");
+  Serial.print((int)md.dx); Serial.print(',');
+  Serial.print((int)md.dy); Serial.println(')');
+
+  // Serial.println(md.max_pix);
+  //delay(100);
+
+
+    distance_x = md.dx; //convTwosComp(md.dx);
+    distance_y = md.dy; //convTwosComp(md.dy);
+
+    distance_xx = 10*distance_x/157;
+    distance_yy = 10*distance_y/157;
+    Serial.println("distance_x = "+String(distance_x));
+    Serial.println("distance_y = "+String(distance_y));
+    Serial.println("distance_xx = "+String(distance_xx));
+    Serial.println("distance_yy = "+String(distance_yy));
+    
+
+total_x1 = (total_x1 + distance_x);
+total_y1 = (total_y1 + distance_y);
+
+total_x = 10*total_x1/157; //Conversion from counts per inch to mm (400 counts per inch)
+total_y = 10*total_y1/157; //Conversion from counts per inch to mm (400 counts per inch)
+
+
+//total_x1_distance = total_x1_distance + abs(distance_x);
+//total_y1_distance = total_y1_distance + abs(distance_y);
+//
+//total_x_distance = 10*total_x1_distance/157; //Conversion from counts per inch to mm (400 counts per inch)
+//total_y_distance = 10*total_y1_distance/157; //Conversion from counts per inch to mm (400 counts per inch)
+//
+//
+//Serial.println("Distance_x_total = " + String(total_x_distance));
+//Serial.println("Distance_y_total = " + String(total_y_distance));
+//Serial.print('\n');
+
+if((distance_xx == 0)&&(distance_yy == 0)){
+  if(_iter == 0){
+    angle_x = prev_xx;
+    angle_y = prev_yy;
+  }
+  _iter = 1;
+}
+
+if(md.motion != 1){
+  _iter = 0;
+}
+    Serial.println("angle_x = "+String(angle_x));
+    Serial.println("angle_y = "+String(angle_y));
+  //counter stuff
+  //min threshold for prev_val needs to be tested based on the maximum speed of the rover
+  if(((200<=prev_val_y)&&(prev_val_y<=208))&&((-208<=total_y)&&(total_y<=-200))){
+    counter_y++;
+  }else if(((-200>=prev_val_y)&&(prev_val_y>=-208))&&((200<=total_y)&&(total_y<=208))){
+     counter_y--;
+  }
+  if(counter_y!=0){
+    if(((0<=prev_val_y)&&(prev_val_y<=8))&&((-8<=total_y)&&(total_y<0))){
+      counter_y--;
+    }else if(((0>=prev_val_y)&&(prev_val_y>=-8))&&((8>=total_y)&&(total_y>0))){
+      counter_y++;
+    }
+  }
+  
+
+  if(((200<=prev_val_x)&&(prev_val_x<=208))&&((-208<=total_x)&&(total_x<=-200))){
+    counter_x++;
+  }else if(((-200>=prev_val_x)&&(prev_val_x>=-208))&&((200<=total_x)&&(total_x<=208))){
+     counter_x--;
+  }
+  if(counter_x!=0){
+    if(((0<=prev_val_x)&&(prev_val_x<=8))&&((-8<=total_x)&&(total_x<0))){
+      counter_x--;
+    }else if(((0>=prev_val_x)&&(prev_val_x>=-8))&&((8>=total_x)&&(total_x>0))){
+      counter_x++;
+    }
+  }
+  
+  //this shows the minimum possible value y can take at the moment 
+  min_y = 208*counter_y;
+  min_x = 208*counter_x;
+  Serial.print("min_x= ");
+  Serial.println(min_x);
+  Serial.print('\n');
+
+    //finding actual value of y 
+  if(counter_y>0){  
+    if(total_y>0){
+      actual_y = min_y + total_y;
+    }else if(total_y<0){
+      actual_y = min_y + (208+total_y);
+    }
+  }else if(counter_y<0){
+    if(total_y>0){
+      actual_y = min_y - (208-total_y);
+    }else if(total_y<0){
+      actual_y = min_y + total_y;
+    }
+  }else{ 
+    actual_y=total_y;
+  }
+
+  if(counter_x>0){  
+    if(total_x>0){
+      actual_x = min_x + total_x;
+    }else if(total_x<0){
+      actual_x = min_x + (208+total_x);
+    }else{
+      //for completeness
+      actual_x = actual_x;
+     }  
+  }else if(counter_x<0){
+    if(total_x>0){
+      actual_x = min_x - (208-total_x);
+    }else if(total_x<0){
+      actual_x = min_x + total_x;
+    }else{
+      //for completeness
+      actual_x = actual_x;
+     }
+  }else{ 
+    actual_x=total_x;
+  }
+
+ Serial.print('\n');
+
+Serial.print("vref = " + String(vref));
+Serial.print("vb = " + String(vb));
+Serial.print('\n');
+
+Serial.println("Distance_x = " + String(total_x));
+Serial.println("Distance_y = " + String(total_y));
+Serial.print('\n');
+
+Serial.print("Counter y= ");
+Serial.println(counter_y);
+Serial.print('\n');
+Serial.print("Counter x= ");
+Serial.println(counter_x);
+Serial.print('\n');
+
+Serial.print('\n');
+Serial.print("Actual y = ");
+Serial.println(actual_y);
+
+Serial.print('\n');
+Serial.print("Actual x = ");
+Serial.println(actual_x);
+
+  delay(100);
+
+  //#endif
+   
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+     char _modenew = 'f';
+     _modenew = Serial.read();
+
+     if(_mode!=_modenew){
+      _mode=_modenew;
+      }
+   }
+
+  digitalWrite(DIRR, DIRRstate);
+  digitalWrite(DIRL, DIRLstate);
+  //*********//
+  //rover_search(STOP);
+  
+ // if(vb >= vref - 0.2){
+//    if(!rover_scan_short(_mode)){
+//      rover_scan_short(_mode);
+//    }
+
+//     if(!rover_scan(_mode)){
+//      rover_scan(_mode);
+//     }
+    
+//   if(_mode == 0){
+//     if(!rover_scan_short(_mode)){
+//      rover_scan_short(_mode);}
+//     Serial1.println("s0 = true") 
+//
+//    }else if(_mode == 1){
+//    
+//    if(found != 5){
+//      
+//      if(!rover_scan(_mode)){
+//        rover_scan(_mode);}
+//        
+//    }else{
+//      brake();
+//     }
+//   }
+//       
+//   if(_mode == 2){
+//    
+//    if(!rover_scan(_mode)){
+//      rover_scan(_mode);}
+//      
+//    if(!reach_forward(_mode)){
+//      reach_forward(_mode);}
+//   }
+//
+//   if(found == 5){
+//    brake();
+//   }
+   
+//  }else{
+ //   brake();}
+    
+  //rover_mode(_mode, STOP);
+  prev_val_y = total_y;
+  prev_val_x = total_x;
+  prev_xx = distance_xx;
+  prev_yy = distance_yy;
+
+  rover_manual(_mode);
+  float angle = store_angle(actual_x);
+  
+  xcoordinatefinder(actual_y_prev, actual_y, angle);
+  ycoordinatefinder(actual_y_prev, actual_y, angle);
+//  if(loop_ite==0){
+//    actual_y_prev = actual_y;
+//  }else{
+//    
+//  }
+  actual_y_prev = actual_y;
+  Serial.println("ACTUAL Y=");
+  Serial.print(actual_y);
+  Serial.println("PREV Y=");
+  Serial.print(actual_y_prev);
+  //loop_ite++;
+  
+  
+  
+}
---
+++ Drive/Combined_cem/functions.h
@@ -0,0 +1,476 @@
+#ifndef functions_h
+#define functions_h
+
+#include <math.h>
+#include <INA219_WE.h>
+INA219_WE ina219;
+
+#define PIN_SS        10
+#define PIN_MISO      12
+#define PIN_MOSI      11
+//#define PIN_SCK       13
+
+#define PIN_MOUSECAM_RESET     8
+#define PIN_MOUSECAM_CS        7
+
+#define ADNS3080_PIXELS_X                 30
+#define ADNS3080_PIXELS_Y                 30
+
+#define ADNS3080_PRODUCT_ID            0x00
+#define ADNS3080_REVISION_ID           0x01
+#define ADNS3080_MOTION                0x02
+#define ADNS3080_DELTA_X               0x03
+#define ADNS3080_DELTA_Y               0x04
+#define ADNS3080_SQUAL                 0x05
+#define ADNS3080_PIXEL_SUM             0x06
+#define ADNS3080_MAXIMUM_PIXEL         0x07
+#define ADNS3080_CONFIGURATION_BITS    0x0a
+#define ADNS3080_EXTENDED_CONFIG       0x0b
+#define ADNS3080_DATA_OUT_LOWER        0x0c
+#define ADNS3080_DATA_OUT_UPPER        0x0d
+#define ADNS3080_SHUTTER_LOWER         0x0e
+#define ADNS3080_SHUTTER_UPPER         0x0f
+#define ADNS3080_FRAME_PERIOD_LOWER    0x10
+#define ADNS3080_FRAME_PERIOD_UPPER    0x11
+#define ADNS3080_MOTION_CLEAR          0x12
+#define ADNS3080_FRAME_CAPTURE         0x13
+#define ADNS3080_SROM_ENABLE           0x14
+#define ADNS3080_FRAME_PERIOD_MAX_BOUND_LOWER      0x19
+#define ADNS3080_FRAME_PERIOD_MAX_BOUND_UPPER      0x1a
+#define ADNS3080_FRAME_PERIOD_MIN_BOUND_LOWER      0x1b
+#define ADNS3080_FRAME_PERIOD_MIN_BOUND_UPPER      0x1c
+#define ADNS3080_SHUTTER_MAX_BOUND_LOWER           0x1e
+#define ADNS3080_SHUTTER_MAX_BOUND_UPPER           0x1e
+#define ADNS3080_SROM_ID               0x1f
+#define ADNS3080_OBSERVATION           0x3d
+#define ADNS3080_INVERSE_PRODUCT_ID    0x3f
+#define ADNS3080_PIXEL_BURST           0x40
+#define ADNS3080_MOTION_BURST          0x50
+#define ADNS3080_SROM_LOAD             0x60
+
+//#define ADNS3080_PRODUCT_ID_VAL        0x17
+
+float open_loop, closed_loop; // Duty Cycles
+float vpd,vb,vref,iL,dutyref,current_mA; // Measurement Variables
+unsigned int sensorValue0,sensorValue1,sensorValue2,sensorValue3;  // ADC sample values declaration
+float ev=0,cv=0,ei=0,oc=0; //internal signals
+float Ts=0.0008; //1.25 kHz control frequency. It's better to design the control period as integral multiple of switching period.
+float kpv=1.5,kiv=5.78,kdv=0; // voltage pid.
+float u0v,u1v,delta_uv,e0v,e1v,e2v; // u->output; e->error; 0->this time; 1->last time; 2->last last time
+float kpi=0.02512,kii=39.4,kdi=0; // current pid.
+float u0i,u1i,delta_ui,e0i,e1i,e2i; // Internal values for the current controller
+float uv_max=4, uv_min=0; //anti-windup limitation
+float ui_max=1, ui_min=0; //anti-windup limitation
+float current_limit = 1.0;
+boolean Boost_mode = 0;
+boolean CL_mode = 0;
+
+unsigned int loopTrigger;
+unsigned int com_count=0;   // a variables to count the interrupts. Used for program debugging.
+
+int DIRRstate = LOW;              //initializing direction states
+int DIRLstate = HIGH;
+
+int DIRL = 20;                    //defining left direction pin
+int DIRR = 21;                    //defining right direction pin
+
+int pwmr = 5;                     //pin to control right wheel speed using pwm
+int pwml = 9;                     //pin to control left wheel speed using pwm
+//*********//
+
+unsigned long ref_time = 0;
+
+float new_origin;
+
+int prev_val_y = 0;
+int prev_val_x = 0;
+int min_y = 0;
+int min_x = 0;
+int actual_y_prev = 0;
+
+int found = 0;
+
+int search_x = 0;
+int search_y = 0;
+int _iter_scan = 0;
+int _iter_short = 0;
+int _iter = 0;
+
+int counter_x = 0;
+int counter_y = 0;
+
+int total_x_distance = 0;
+int total_y_distance = 0;
+int total_x = 0;
+int total_y = 0;
+int total_x1_distance = 0;
+int total_y1_distance = 0;
+int total_x1 = 0;
+int total_y1 = 0;
+float xcal_total = 0;
+float ycal_total = 0;
+
+int loop_ite=0;
+int x=0;
+int y=0;
+
+int a=0;
+int b=0;
+
+int actual_x_short = 0;
+int actual_x_scan = 0;
+int actual_y = 0;
+int actual_x = 0;
+
+float distance_x=0;
+float distance_y=0;
+float distance_xx=0;
+float distance_yy=0;
+float prev_xx = 0;
+float prev_yy = 0;
+float angle_x = 0;
+float angle_y = 0;
+
+int set_yf = -300; //-ve
+int set_yb = 300; //+ve
+int set_xr = -300; //-ve
+int set_xl = 300; //+ve
+
+volatile byte movementflag=0;
+volatile int xydat[2];
+
+int tdistance = 0;
+
+void mousecam_write_reg(int reg, int val)
+{
+  digitalWrite(PIN_MOUSECAM_CS, LOW);
+  SPI.transfer(reg | 0x80);
+  SPI.transfer(val);
+  digitalWrite(PIN_MOUSECAM_CS,HIGH);
+  delayMicroseconds(50);
+}
+
+int mousecam_init()
+{
+  pinMode(PIN_MOUSECAM_RESET,OUTPUT);
+  pinMode(PIN_MOUSECAM_CS,OUTPUT);
+  
+  digitalWrite(PIN_MOUSECAM_CS,HIGH);
+
+  // turn on sensitive mode
+  mousecam_write_reg(ADNS3080_CONFIGURATION_BITS, 0x19);
+
+  return 0;
+}
+
+int mousecam_read_reg(int reg)
+{
+  digitalWrite(PIN_MOUSECAM_CS, LOW);
+  SPI.transfer(reg);
+  delayMicroseconds(75);
+  int ret = SPI.transfer(0xff);
+  digitalWrite(PIN_MOUSECAM_CS,HIGH); 
+  delayMicroseconds(1);
+  return ret;
+}
+
+struct MD
+{
+ byte motion;
+ char dx, dy;
+ byte squal;
+ word shutter;
+ byte max_pix;
+};
+
+
+void mousecam_read_motion(struct MD *p)
+{
+  digitalWrite(PIN_MOUSECAM_CS, LOW);
+  SPI.transfer(ADNS3080_MOTION_BURST);
+  delayMicroseconds(75);
+  p->motion =  SPI.transfer(0xff);
+  p->dx =  SPI.transfer(0xff);
+  p->dy =  SPI.transfer(0xff);
+  p->squal =  SPI.transfer(0xff);
+  p->shutter =  SPI.transfer(0xff)<<8;
+  p->shutter |=  SPI.transfer(0xff);
+  p->max_pix =  SPI.transfer(0xff);
+  digitalWrite(PIN_MOUSECAM_CS,HIGH); 
+  delayMicroseconds(5);
+}
+
+ISR(TCA0_CMP1_vect){
+  TCA0.SINGLE.INTFLAGS |= TCA_SINGLE_CMP1_bm; //clear interrupt flag
+  loopTrigger = 1;
+}
+
+// This subroutine processes all of the analogue samples, creating the required values for the main loop
+
+float saturation( float sat_input, float uplim, float lowlim){ // Saturatio function
+  if (sat_input > uplim) sat_input=uplim;
+  else if (sat_input < lowlim ) sat_input=lowlim;
+  else;
+  return sat_input;
+}
+
+void pwm_modulate(float pwm_input){ // PWM function
+  analogWrite(6,(int)(255-pwm_input*255)); 
+}
+
+// This is a PID controller for the voltage
+
+float pidv( float pid_input){
+  float e_integration;
+  e0v = pid_input;
+  e_integration = e0v;
+ 
+  //anti-windup, if last-time pid output reaches the limitation, this time there won't be any intergrations.
+  if(u1v >= uv_max) {
+    e_integration = 0;
+  } else if (u1v <= uv_min) {
+    e_integration = 0;
+  }
+
+  delta_uv = kpv*(e0v-e1v) + kiv*Ts*e_integration + kdv/Ts*(e0v-2*e1v+e2v); //incremental PID programming avoids integrations.there is another PID program called positional PID.
+  u0v = u1v + delta_uv;  //this time's control output
+
+  //output limitation
+  saturation(u0v,uv_max,uv_min);
+  
+  u1v = u0v; //update last time's control output
+  e2v = e1v; //update last last time's error
+  e1v = e0v; // update last time's error
+  return u0v;
+}
+
+// This is a PID controller for the current
+
+float pidi(float pid_input){
+  float e_integration;
+  e0i = pid_input;
+  e_integration=e0i;
+  
+  //anti-windup
+  if(u1i >= ui_max){
+    e_integration = 0;
+  } else if (u1i <= ui_min) {
+    e_integration = 0;
+  }
+  
+  delta_ui = kpi*(e0i-e1i) + kii*Ts*e_integration + kdi/Ts*(e0i-2*e1i+e2i); //incremental PID programming avoids integrations.
+  u0i = u1i + delta_ui;  //this time's control output
+
+  //output limitation
+  saturation(u0i,ui_max,ui_min);
+  
+  u1i = u0i; //update last time's control output
+  e2i = e1i; //update last last time's error
+  e1i = e0i; // update last time's error
+  return u0i;
+}
+
+void sampling(){
+
+  // Make the initial sampling operations for the circuit measurements
+  
+  sensorValue0 = analogRead(A0); //sample Vb
+  vref = 1.8;
+  sensorValue3 = analogRead(A3); //sample Vpd
+  current_mA = ina219.getCurrent_mA(); // sample the inductor current (via the sensor chip)
+
+  // Process the values so they are a bit more usable/readable
+  // The analogRead process gives a value between 0 and 1023 
+  // representing a voltage between 0 and the analogue reference which is 4.096V
+  
+  vb = sensorValue0 * (4.096 / 1023.0); // Convert the Vb sensor reading to volts
+  sensorValue2 = vref * (1023.0 / 4.096);
+  //vref = sensorValue2 * (4.096 / 1023.0); // Convert the Vref sensor reading to volts
+  vpd = sensorValue3 * (4.096 / 1023.0); // Convert the Vpd sensor reading to volts
+
+  // The inductor current is in mA from the sensor so we need to convert to amps.
+  // We want to treat it as an input current in the Boost, so its also inverted
+  // For open loop control the duty cycle reference is calculated from the sensor
+  // differently from the Vref, this time scaled between zero and 1.
+  // The boost duty cycle needs to be saturated with a 0.33 minimum to prevent high output voltages
+  
+  if (Boost_mode == 1){
+    iL = -current_mA/1000.0;
+    dutyref = saturation(sensorValue2 * (1.0 / 1023.0),0.99,0.33);
+  }else{
+    iL = current_mA/1000.0;
+    dutyref = sensorValue2 * (1.0 / 1023.0);
+  }
+  
+}
+
+void back(){
+  digitalWrite(pwmr,HIGH);
+  digitalWrite(pwml,HIGH);
+  DIRRstate = HIGH;
+  DIRLstate = LOW;
+}
+
+void left(){
+  digitalWrite(pwmr,HIGH);
+  digitalWrite(pwml,HIGH);
+  DIRRstate = LOW;
+  DIRLstate = LOW;
+}
+
+void right(){
+  digitalWrite(pwmr,HIGH);
+  digitalWrite(pwml,HIGH);
+  DIRRstate = HIGH;
+  DIRLstate = HIGH;
+}
+
+void forward(){
+  digitalWrite(pwmr,HIGH);
+  digitalWrite(pwml,HIGH);
+  DIRRstate = LOW;
+  DIRLstate = HIGH;
+}
+
+void brake(){
+  digitalWrite(pwmr,LOW);
+  digitalWrite(pwml,LOW);
+}
+
+//**********************************//
+float store_angle(float x){
+    float angle_deg = ((x/850)*360);
+    float angle_rad = (x/850)*2*PI;
+    Serial.println("angle = ");
+    Serial.print(angle_deg);
+    return angle_rad;
+}
+
+float xcoordinatefinder(float old_r, float new_r, float angle){
+  //if(loop_ite==0){
+    //float old_x = old_r*cos(angle);
+    float xcal = (new_r - old_r)*cos(angle);
+    //float new_x = old_x + xcal;
+    xcal_total = xcal_total + xcal;
+    Serial.print("x coordinate of rover=");
+    Serial.print(xcal_total);
+    return xcal_total;
+  
+}
+float ycoordinatefinder(float old_r, float new_r, float angle){
+  //if(total_y != prev_val_y){
+    //float old_y = old_r*sin(angle);
+    float ycal = (new_r - old_r)*sin(angle);
+    //float new_y = old_y + ycal;
+    ycal_total = ycal_total + ycal;
+    Serial.println("y coordinate of rover=");
+    Serial.print(ycal_total);
+    return ycal_total;
+  
+}
+void gotocoordinate(int x, int y){
+float angle = arctan2(y,x);
+float r = sqrt((x^2)+(y^2));
+// devamini yarin yaz
+}
+
+
+bool rover_scan_short(char _mode){
+  if(_iter_short == 0){
+    actual_x_short = abs(actual_x);
+  }
+  if(abs(actual_x) >= 350+actual_x_short){
+    brake();
+    return true;
+  }else if(_mode == 's'){
+    brake();
+    if(_iter_short == 1){
+      ref_time = millis();
+    }
+    _iter_short = 2;
+    Serial.println("ref_time = "+String(ref_time));
+    if((millis() - ref_time) >= 1000){
+      brake();
+      _iter_short == 3;
+    }else{
+      forward();
+    }
+    if(_iter_short == 3){
+      if((millis() - ref_time) >= 2000){
+        brake();
+      }else{
+        back();
+    }
+    }
+  }else{
+    right();
+    _iter_short = 1;
+    return false;
+  }
+}
+
+bool rover_scan(char _mode){
+  if(_iter_scan == 0){
+    actual_x_scan = abs(actual_x);
+  }
+  if((abs(actual_x) >= 350+actual_x_scan)||(_mode == 's')){
+    brake();
+    store_angle(actual_x);
+    return true;
+  }else{
+    right();
+    _iter_scan = 1;
+    return false;
+  }
+}
+
+
+void rover_manual(char _mode){
+    if (_mode == 'w') {
+    forward();
+    } 
+  //rotating clockwise
+  if (_mode == 'd') {
+    right();
+  
+  }
+  if (_mode == 's') {
+    back();
+     
+  }
+  //rotating anticlockwise
+  if (_mode == 'a') {
+    left();
+  } 
+  if(_mode == 'x'){
+  brake();}
+}
+
+bool reach_forward(char _mode){
+    if(_mode == 'g'){
+      forward();
+      vref = 3;
+      return false;
+    }else if(_mode == 's'){
+        brake();
+        return true;
+    }else if(_mode == 'r'){
+        right();
+        vref = 1.7;
+        reach_forward('g');
+    }else if(_mode == 'l'){
+        left();
+        vref = 1.7;
+        reach_forward('g');
+    }
+}
+
+void rover_mode(char _mode, bool STOP){
+  if (_mode == 'n'){
+    //rover_scan(STOP);
+  }else if(_mode == 'm'){
+    rover_manual(_mode);
+  }
+}
+
+#endif 
